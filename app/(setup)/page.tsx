154a19be57269ef9020afe372fc60aee8a339bad
InsidiousDen <boichuk.db@gmail.com>
2023-09-14 08:15:43 +0300

    initial-modal

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,3 +1,4 @@
+import { InitialModal } from "@/components/modals/initial-modal";
 import { db } from "@/lib/db";
 import { initialProfile } from "@/lib/initial-profile";
 import { redirect } from "next/navigation";
@@ -18,11 +19,7 @@ const SetupPage = async () => {
     return redirect(`/servers/${server.id}`);
   }
 
-  return (
-    <div>
-      <h1>Create a Server</h1>
-    </div>
-  );
+  return <InitialModal />;
 };
 
 export default SetupPage;
---
+++ components/modals/initial-modal.tsx
@@ -0,0 +1,114 @@
+"use client";
+
+import * as zod from "zod";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useEffect, useState } from "react";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+export const InitialModal = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  const formSchema = zod.object({
+    name: zod.string().nonempty({
+      message: "Please enter a server name",
+    }),
+    imageUrl: zod.string().nonempty({
+      message: "Please add server image",
+    }),
+  });
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      imageUrl: "",
+    },
+    mode: "onChange",
+  });
+
+  const { control, handleSubmit, formState } = form;
+
+  const isLoading = formState.isSubmitting;
+
+  const onSubmit = async (values: zod.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <Dialog open>
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6 ">
+          <DialogTitle className="text-2xl font-bold text-center">
+            Customize your server
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            Give your server a personality with a name and an icon.
+            <br /> You can always change it later.
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-8">
+            <div className="space-y-8 px-6">
+              <div className="flex items-center justify-center text-center ">
+                TODO: Add image upload
+              </div>
+              <FormField
+                control={control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
+                      Server Name
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={isLoading}
+                        className="bg-zinc-300/50 border-0 focus-visible:ring-0 focus-visible:ring-offset-0"
+                        placeholder="Enter a server name"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <DialogFooter className="bg-grey-100 px-6 py-4">
+              <Button disabled={isLoading} variant="primary">
+                Create
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
