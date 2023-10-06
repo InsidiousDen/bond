96ef1b8f2192ee5e78e192a036660a0e9a1ffbfc
InsidiousDen <boichuk.db@gmail.com>
2023-10-06 13:36:25 +0200

    server-channel-page

---
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -0,0 +1,43 @@
+import { ChatHeader } from "@/components/chat/chat-header";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+import { redirectToSignIn } from "@clerk/nextjs";
+
+interface ChannelIdPageProps {
+  params: { serverId: string; channelId: string };
+}
+
+const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
+  const profile = await currentProfile();
+  if (!profile) return redirectToSignIn();
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId,
+    },
+  });
+
+  const member = await db.member.findFirst({
+    where: {
+      profileId: profile.id,
+      serverId: params.serverId,
+    },
+  });
+
+  if (!channel || !member) {
+    redirect("/");
+  }
+
+  return (
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader
+        serverId={channel.serverId}
+        name={channel.name}
+        type="channel"
+      />
+    </div>
+  );
+};
+
+export default ChannelIdPage;
